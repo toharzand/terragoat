86c9cd336474bd42641da520aec2590af4680156
toharzand <89066773+toharzand@users.noreply.github.com>
2021-08-17 12:08:55 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "data" {
+  
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
@@ -25,6 +26,7 @@ resource "aws_s3_bucket" "data" {
 
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
+  
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
   tags = {
