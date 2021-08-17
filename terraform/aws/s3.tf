f6f41143f271d52b7b926d78490c03c1cc2f46b3
toharzand <89066773+toharzand@users.noreply.github.com>
2021-08-17 19:53:12 +0300

    Update terraform/aws/s3.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -142,4 +142,11 @@ resource "aws_s3_bucket" "logs" {
 }
 resource "aws_s3_bucket" "logs2" {
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
